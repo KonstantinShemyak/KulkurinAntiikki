4a343a4899dd3287ded863e0f4edb8792bdaaeb5
Konstantin Shemyak <konstantin@shemyak.com>
2025-06-14 12:52:59 +0300

    chore: cosmetic cleanup of JS

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -1,12 +1,9 @@
-console.log("tervetuloa");
-
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
-//
 let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 
@@ -25,6 +22,5 @@ function toggleMenu () {
     menuBranding.classList.remove("show");
     navItems.forEach((item) => item.classList.remove("show"));
     showMenu = false;
-    
   }
 };
